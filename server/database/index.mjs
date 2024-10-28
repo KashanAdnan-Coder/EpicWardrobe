3b0e1b4ad282edd8a2cad9a0fb7bc6a89b4c4f1a
KashanAdnan-Coder <computertechhub0@gmail.com>
2024-10-28 15:56:22 +0500

    error-resolved-from-mongodb

--- server/database/index.mjs
+++ server/database/index.mjs
@@ -1,17 +1,17 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGDB_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
 
-async function connectDatabase() {
+async function connectDatabase(URI) {
+    const client = new MongoClient(URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
     try {
         await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        await client.db().command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         await client.close();
--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,11 @@
 import express from "express"
-import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+import connectDatabase from "./database/index.mjs"
 
 const app = express()
-mongoose.connect()
 const PORT = process.env.PORT || 3000
+connectDatabase(process.env.MONGODB_URI)
 
 app.listen(PORT, () => {
     console.log(`Server is listening ${PORT}`);
